6a110439792030dc9e0fb734521220b90272e74e
oxgen1987 <oxgen1987@outlook.com>
2019-09-27 10:42:07 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const style = {
+      backgroundColor: 'black',
+      padding: '36px',
+      color: 'white',
+      fontSize: '12px'
+    };
+
+    return (
+      <div style={style}>
+        hi there
+      </div>
+    );
+  }
+}
+
+export default App;
